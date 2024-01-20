6b1b4cf0619ffe82e492f92b588b73ad5c4b4761
Ruan Portella <ruanmorales29@gmail.com>
2024-01-20 16:40:43 -0300

    Feat: Cria UserController

---
+++ server/src/modules/user/User.controller.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import UserService from "./implementations/User.service";
+
+const userService = new UserService();
+
+export default class UserController {
+
+    async create(request: Request, response: Response): Promise<Response> {
+        const { name, email, password, telephone, profileImage } = request.body;
+
+        const user = await userService.create({ name, email, password, telephone, profileImage });
+
+        return response.status(user.status).json(user.data);
+    }
+
+    async login(request: Request, response: Response): Promise<Response> {
+        const { email, password } = request.body;
+
+        const user = await userService.login({ email, password });
+
+        return response.status(user.status).json(user.data);
+    }
+}
