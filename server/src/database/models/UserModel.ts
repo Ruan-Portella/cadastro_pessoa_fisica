73271fe12d0ef9938be740a8bbc5a6c9e9c9160d
Ruan Portella <ruanmorales29@gmail.com>
2024-01-20 16:39:38 -0300

    Feat: Cria Model user

---
+++ server/src/database/models/UserModel.ts
@@ -0,0 +1,57 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from "sequelize";
+import db from ".";
+
+class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
+  declare id: CreationOptional<string>;
+  declare name: string;
+  declare email: string;
+  declare password: CreationOptional<string>;
+  declare telephone: string;
+  declare profileImage: string;
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      unique: true,
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    telephone: {
+      unique: true,
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    profileImage: {
+      allowNull: true,
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize: db,
+    modelName: "users",
+    timestamps: false,
+  }
+);
+
+export default User;
