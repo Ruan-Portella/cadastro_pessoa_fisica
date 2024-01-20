25b06aec9a130d1d3a015824f7508fe8b29eef79
Ruan Portella <ruanmorales29@gmail.com>
2024-01-20 18:41:19 -0300

    Feat: Cria imagens

---
+++ client/src/assets/Add.tsx
@@ -0,0 +1,11 @@
+export default function Add({className}: {
+    className: string,
+  }) {
+  return (
+    <svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className={`${className}`}>
+        <g id="Edit / Add_Plus">
+            <path id="Vector" d="M6 12H12M12 12H18M12 12V18M12 12V6" stroke="#E44449" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </g>
+    </svg>
+  )
+}
