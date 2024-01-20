3816b624510cc733dac5d807cfdc0ac4560fc0f4
Ruan Portella <ruanmorales29@gmail.com>
2024-01-20 16:21:30 -0300

    Feat: Cria rotas

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,12 @@
+import { Route, Routes } from 'react-router'
+import Login from './pages/Login'
+
 function App() {
 
   return (
-    <>
-    </>
+    <Routes>
+      <Route path='/' element={<Login />}/>
+    </Routes>
   )
 }
 
