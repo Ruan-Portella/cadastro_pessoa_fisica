051f911d21d5196e39a46afbbeabc559a623e95d
Ruan Portella <ruanmorales29@gmail.com>
2024-01-21 13:59:07 -0300

    Feat: Cria interface Address e Contact

---
+++ server/src/database/Interfaces/IAddressModel.ts
@@ -0,0 +1,10 @@
+export default interface IAddress {
+  id: string;
+  street: string;
+  number: string;
+  complement: string;
+  city: string;
+  state: string;
+  zipCode: string;
+  privatePersonId: string;
+}
---
+++ server/src/database/Interfaces/IContactsModel.ts
@@ -0,0 +1,7 @@
+export default interface IAddress {
+  id: string;
+  name: string;
+  typeContact: string;
+  contact: string;
+  privatePersonId: string;
+}
