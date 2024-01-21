9bce7ba45ad9695dad03dace5faae3b1843493e9
Ruan Portella <ruanmorales29@gmail.com>
2024-01-21 01:50:42 -0300

    Feat: Cria ContactModal

---
+++ client/src/components/ContactsModal.tsx
@@ -0,0 +1,71 @@
+import InputField from './InputField';
+import Button from './Button';
+
+type Contact = {
+  typeContact: string,
+  name: string,
+  contact: string,
+}
+
+type ContactsModalProps = {
+  isError: string
+  index: number
+  contacts: Contact
+  onClick?: () => void,
+  count: number,
+  setContacts: (value: Contact[] | ((prevContacts: Contact[]) => Contact[])) => void
+}
+
+export default function ContactsModal({ isError, index, contacts, count, setContacts }: ContactsModalProps) {
+
+  const addContact = () => {
+    setContacts((prevContacts: Contact[]) => [
+      ...prevContacts,
+      {
+        typeContact: '',
+        name: '',
+        contact: '',
+      }
+    ]);
+  };
+
+  const removeContacts = (index: number) => {
+    setContacts((prevContacts) => {
+      const novosAdresses = [...prevContacts];
+      novosAdresses.splice(index, 1);
+      return novosAdresses;
+    });
+  };
+
+  const handleContacts = (index: number, campo: keyof Contact, valor: string) => {
+    setContacts((prevContacts) => {
+      const novosAdresses = [...prevContacts];
+      novosAdresses[index][campo] = valor;
+      return novosAdresses;
+    });
+  };
+
+  return (
+    <div  key={`${index+count}-contact`}>
+      <div className='flex flex-wrap gap-x-2 gap-y-2'>
+        <InputField disabled={index + 1 !== count} labelClassName='text-black' label="Nome" placeholder="Digite seu nome" className='xl:w-[32.9%] sm:w-[49%] max-sm:w-ful' id='name' onChange={(e) => handleContacts(index, 'name', e.target.value)} type='text' value={contacts.name} errorMessage='Nome invalido' isError={isError === 'name' ? true : false} />
+        <InputField disabled={index + 1 !== count}  labelClassName='text-black' label="Contato" placeholder="Digite seu email ou numero" className='xl:w-[32.9%] sm:w-[49%] max-sm:w-full' id='type' onChange={(e) => handleContacts(index, 'contact', e.target.value)} type='text' value={contacts.contact} errorMessage='Tipo invalido' isError={isError === 'contact' ? true : false} />
+        <InputField disabled={index + 1 !== count}  labelClassName='text-black' label="Tipo de Contato" placeholder="Digite o tipo de contato" className='xl:w-[32.9%] sm:w-[49%] max-sm:w-full' id='type' onChange={(e) => handleContacts(index, 'typeContact', e.target.value)} type='text' value={contacts.typeContact} errorMessage='Tipo invalido' isError={isError === 'type' ? true : false} />
+      </div>
+      {
+        index + 1 === count ? (
+          <Button type='button' title='Adicionar novo contato' className='flex bg-black text-white w-1/6 text-[14px] justify-center hover:bg-slate-700' onClick={addContact}/>
+        ) : (
+          <div className='flex justify-end'>
+             <Button type='button' title='Remover Contato' className='flex bg-black text-white w-1/6 text-[14px] justify-center hover:bg-slate-700' onClick={() => removeContacts(index)}/>
+          </div>
+        )
+      }
+      <div className='mt-4'>
+        <svg height="1" width="100%">
+          <line x1="0" y1="0" x2="100%" y2="0" stroke="black" strokeWidth="2" />
+        </svg>
+      </div>
+    </div>
+  )
+}
